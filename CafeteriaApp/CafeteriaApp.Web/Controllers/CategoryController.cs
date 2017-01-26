f98f19a03c007b49fcd06d82c228226a05b6e46b
Eng-EsraHosny <esraahosny292@yahoo.com>
2017-01-26 18:56:14 +0200

    Revert "Revert "Addition controller ready""
    This reverts commit 737e439a5e19e1c3e1cdb368ad155664ef25f7b7.

--- CafeteriaApp/CafeteriaApp.Web/Controllers/CategoryController.cs
+++ CafeteriaApp/CafeteriaApp.Web/Controllers/CategoryController.cs
@@ -6,47 +6,122 @@ using System.Net.Http;
 using System.Web.Http;
 using CafeteriaApp.Data.Contexts;
 using CafeteriaApp.Data.Models;
+using CafeteriaApp.Web.Models;
 
 namespace CafeteriaApp.Web.Controllers
 {
     public class CategoryController : ApiController
     {
+
         public AppDb appdb = new AppDb();
-        public IHttpActionResult GetCategory(int id)
-        {
-            var category = appdb.Categories.FirstOrDefault(c => c.Id == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-            return Ok(category);
-        }
 
-        public ICollection<Category> GetAllCategories()
-        {
-            return appdb.Categories.ToList();
-        }
+        //public IHttpActionResult GetAllCategories()
+        //{
+        //    var category = appdb.Categories.Select(category1 => new CategoryViewModel()
+        //    {
+        //        CafeteriaId = category1.CafeteriaId,
+        //        Id = category1.Id,
+        //        Name = category1.Name,
+        //        Cafeteria = new CafeteriaViewModel()
+        //        {
+        //            Name = category1.Cafeteria.Name,
+        //            Id = category1.Cafeteria.Id,
+        //            CafeteriaId = category1.CafeteriaId,
+        //        }
+        //    }).ToList();
 
-        public IHttpActionResult AddCategory(Category c)
-        {
-            if (c != null)
-            {
-                appdb.Categories.Add(c);
-                appdb.SaveChanges();
-                return Ok(c);
-            }
-            else
-            {
-                return BadRequest();
-            }
-        }
+        //    return Ok(category);
+        //}
 
-        public IHttpActionResult DeleteCategory(int id)
+        //public IHttpActionResult Get(int id)
+        //{
+        //    var cate = appdb.Categories.FirstOrDefault(c => c.Id == id);
+        //    if (cate == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    CategoryViewModel cat = new CategoryViewModel()
+        //    {
+        //        Id = cate.Id,
+        //        Name = cate.Name,
+        //        Cafeteria = new CafeteriaViewModel()
+        //        {
+        //            Name = cate.Cafeteria.Name,
+        //            Id = cate.Cafeteria.Id,
+        //            CafeteriaId = Cate.Cafeteria.CafeteriaId,
+        //        }
+        //    };
+        //    return Ok(cat);
+
+
+        //}
+
+
+
+        //[HttpPost]
+        //public IHttpActionResult Add(Category c)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest("Invalid data.");
+        //    }
+        //    var m = appdb.Categories.Add(new Category()
+        //    {
+        //        Id = c.Id,
+        //        Name = c.Name,
+         //         Cafeteria = new CafeteriaViewModel()
+        //        {
+        //            Name = cate.Cafeteria.Name,
+        //            Id = cate.Cafeteria.Id,
+        //            CafeteriaId = Cate.Cafeteria.CafeteriaId,
+        //        }
+
+        //    });
+        //    appdb.SaveChanges();
+        //    return Ok();
+        //}
+
+        //[HttpPut]
+        //public IHttpActionResult PUT(CategoryViewModel c)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest("Not a valid data");
+        //    }
+
+        //    var existingCategory= appdb.Categories.Where(x => x.Id == c.Id).FirstOrDefault<Category>();
+
+        //    if (existingCategory != null)
+        //    {
+        //        existingCategory.Id = c.Id;
+        //        existingCategory.Name = c.Name;
+        //        Cafeteria = new CafeteriaViewModel()
+        //        {
+        //            Name = existingCategory.Cafeteria.Name,
+        //            Id = existingCategory.Cafeteria.Id,
+        //            CafeteriaId = existingCategory.Cafeteria.CafeteriaId,
+        //        }
+        //       appdb.SaveChanges();
+        //    }
+        //    else
+        //    {
+        //        return NotFound();
+        //    }
+
+
+        //    return Ok();
+        //}
+
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
         {
-            var categoryToDelete = appdb.Categories.FirstOrDefault(c => c.Id == id);
-            if (categoryToDelete != null)
+            var categoryDeleted = appdb.Categories.FirstOrDefault(d => d.Id == id);
+            
+            if (categoryDeleted != null)
             {
-                appdb.Categories.Remove(categoryToDelete);
+                appdb.Categories.Remove(categoryDeleted);
                 appdb.SaveChanges();
                 return Ok();
             }
@@ -55,5 +130,7 @@ namespace CafeteriaApp.Web.Controllers
                 return NotFound();
             }
         }
+
+
     }
 }
