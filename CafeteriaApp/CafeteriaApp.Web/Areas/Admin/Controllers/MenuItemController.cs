ef81aadd9b6b31469f012daf2abeb0681272dde6
MohamedHemdan22 <Mohamed.h.22@outlook.com>
2017-01-26 15:42:10 +0200

    ADD CAFTERIA

--- CafeteriaApp/CafeteriaApp.Web/Areas/Admin/Controllers/MenuItemController.cs
+++ CafeteriaApp/CafeteriaApp.Web/Areas/Admin/Controllers/MenuItemController.cs
@@ -9,20 +9,21 @@ namespace CafeteriaApp.Web.Areas.Admin.Controllers
     public class MenuItemController : Controller
     {
         // GET: Admin/MenuItem
-        public ActionResult Index()
+        public ActionResult Index() // getall
         {
             return View();
         }
 
         // GET: Admin/MenuItem/Create
-        public ActionResult Create()
+        public ActionResult Create() // add
         {
             return View();
         }
 
         // GET: Admin/MenuItem/Details/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int id) //edit
         {
+            ViewBag.Id = id;
             return View();
         }
 
