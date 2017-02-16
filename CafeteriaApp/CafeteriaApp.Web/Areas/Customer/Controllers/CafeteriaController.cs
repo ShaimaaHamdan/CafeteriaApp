25e6253207518bc35aef610f082c98c7ff871507
mostafaelsayed <test@youremail.com>
2017-02-16 15:48:37 +0200

    adding cafeteria and dashboard controller to customer area

---
+++ CafeteriaApp/CafeteriaApp.Web/Areas/Customer/Controllers/CafeteriaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CafeteriaApp.Web.Areas.Customer.Controllers
+{
+    public class CafeteriaController : Controller
+    {
+        // GET: Customer/Cafeteria
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Customer/Cafeteria/5
+        public ActionResult show(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
