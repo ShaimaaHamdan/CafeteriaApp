804ea3bbd9bfd6b752ed6bf4531870281a1a7a79
MohamedHemdan22 <Mohamed.h.22@outlook.com>
2017-02-19 20:36:41 +0200

    add missing files

---
+++ CafeteriaApp/CafeteriaApp.Data/Migrations/201702171652562_EditeInUserEntity.cs
@@ -0,0 +1,20 @@
+namespace CafeteriaApp.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EditeInUserEntity : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "AccessFailedCount", c => c.Int(nullable: false));
+            DropColumn("dbo.AspNetUsers", "IAccessFailedCountd");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.AspNetUsers", "IAccessFailedCountd", c => c.Int(nullable: false));
+            DropColumn("dbo.AspNetUsers", "AccessFailedCount");
+        }
+    }
+}
