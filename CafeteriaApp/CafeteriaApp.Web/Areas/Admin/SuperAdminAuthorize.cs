57a4bd68fa91c51a2832d53a1771f5edb710e470
Shaimaa Hamdan <Shaimaa.Hamdan@gmail.com>
2017-02-20 10:28:50 +0100

    Adding Ignored files

---
+++ CafeteriaApp/CafeteriaApp.Web/Areas/Admin/SuperAdminAuthorize.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CafeteriaApp.Web.Areas.Admin
+{
+    public class SuperAdminAuthorize : AuthorizeAttribute
+    {
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("~/Admin/Login/Login");
+        }
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (this.AuthorizeCore(filterContext.HttpContext))
+            {
+                base.OnAuthorization(filterContext);
+            }
+            else
+            {
+                this.HandleUnauthorizedRequest(filterContext);
+            }
+        }
+    }
+}
