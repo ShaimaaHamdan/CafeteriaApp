a00980c6471b17340ca83d6ff226dcf1b249dd91
Marco Emile <marco267@yahoo.com>
2017-03-02 17:30:48 +0200

    Create Category & MenuItem Controllers, Views in Customer Area.
    Create Category & MenuItem Controllers, Views in Customer Area ..Change
    "Show" view of Cafeteria & Create "Show" view in Category.

---
+++ CafeteriaApp/CafeteriaApp.Web/Areas/Customer/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CafeteriaApp.Web.Areas.Customer.Controllers
+{
+    public class CategoryController : Controller
+    {
+     
+        // GET: Customer/Category/show/5
+
+        //public ActionResult Show(int id)
+        //{
+        //    ViewBag.Id = id;
+        //    return View();
+        //}
+
+        public ActionResult Show(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+
+
+    }
+}
