e6b0327cda6d535797e5608d6e517c53d4858ade
mostafaelsayed <test@youremail.com>
2017-03-04 10:37:03 +0100

    adding order controller and views
    order controller in customer area
    adding create and index view in order
    javascript for order and orderitem

---
+++ CafeteriaApp/CafeteriaApp.Web/Areas/Customer/Controllers/OrderController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CafeteriaApp.Web.Areas.Customer.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Customer/Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //// GET: Customer/Order/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        // GET: Customer/Order/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    //    //// POST: Customer/Order/Create
+    //    //[HttpPost]
+    //    //public ActionResult Create(FormCollection collection)
+    //    //{
+    //    //    try
+    //    //    {
+    //    //        // TODO: Add insert logic here
+
+    //    //        return RedirectToAction("Index");
+    //    //    }
+    //    //    catch
+    //    //    {
+    //    //        return View();
+    //    //    }
+    //    //}
+
+    //    // GET: Customer/Order/Edit/5
+    //    public ActionResult Edit(int id)
+    //    {
+    //        return View();
+    //    }
+
+    //    // POST: Customer/Order/Edit/5
+    //    [HttpPost]
+    //    public ActionResult Edit(int id, FormCollection collection)
+    //    {
+    //        try
+    //        {
+    //            // TODO: Add update logic here
+
+    //            return RedirectToAction("Index");
+    //        }
+    //        catch
+    //        {
+    //            return View();
+    //        }
+    //    }
+
+    //    // GET: Customer/Order/Delete/5
+    //    public ActionResult Delete(int id)
+    //    {
+    //        return View();
+    //    }
+
+    //    // POST: Customer/Order/Delete/5
+    //    [HttpPost]
+    //    public ActionResult Delete(int id, FormCollection collection)
+    //    {
+    //        try
+    //        {
+    //            // TODO: Add delete logic here
+
+    //            return RedirectToAction("Index");
+    //        }
+    //        catch
+    //        {
+    //            return View();
+    //        }
+    //    }
+    }
+}
