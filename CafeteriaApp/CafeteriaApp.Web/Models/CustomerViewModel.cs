9a857fd63730108a0ed89ec5732032efb490d2a2
MohamedHemdan <Mohamed.h.22@outlook.com>
2017-04-07 04:39:04 -0700

    add DependentViewModel

--- CafeteriaApp/CafeteriaApp.Web/Models/CustomerViewModel.cs
+++ CafeteriaApp/CafeteriaApp.Web/Models/CustomerViewModel.cs
@@ -17,11 +17,13 @@ namespace CafeteriaApp.Web.Models
 
         public UserViewModel User { get; set; }
 
-        public List<OrderViewModel> Orders { get; internal set; }
+        public List<DependentViewModel> Dependents { get; set; }
 
-        public List<MenuItemViewModel> Favourites { get; internal set; }
+        public List<OrderViewModel> Orders { get;  set; }
 
-        public List<MenuItemViewModel> Restricts { get; internal set; }
+        public List<MenuItemViewModel> Favourites { get;  set; }
+
+        public List<MenuItemViewModel> Restricts { get;  set; }
             
     }
 }
---
+++ CafeteriaApp/CafeteriaApp.Web/Models/DependentViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CafeteriaApp.Web.Models
+{
+    public class DependentViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public double DependentCredit { get; set; }
+
+        public double DayLimit { get; set; }
+
+        public string SchoolYear { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public string Image { get; set; }
+
+        public CustomerViewModel Customer { get; set; } 
+    }
+}
