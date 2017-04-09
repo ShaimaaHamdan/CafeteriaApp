6a811a58afec1215ae0ce0c135d29c46c5fdaefc
MohamedHemdan <Mohamed.h.22@outlook.com>
2017-04-09 14:50:10 +0200

    fix modal and content view

--- CafeteriaApp/CafeteriaApp.Web/Scripts/Customer/menuitem.js
+++ CafeteriaApp/CafeteriaApp.Web/Scripts/Customer/menuitem.js
@@ -1,12 +1,38 @@
 ﻿function CustomerMenuItemViewModel(id) {
     var self = this;
     self.categoryId = ko.observable(id);
+    self.customerId = ko.observable(1);
     self.menuItemId = ko.observable();
     self.orderId = ko.observable();
     self.menuItems = ko.observableArray();
     self.cafeteriaId = ko.observable();
     self.name = ko.observable();
+    
+    self.model = ko.validatedObservable({
+        firstName: ko.observable().extend({ required: true, maxLength: 100 }),
+        lastName: ko.observable().extend({ required: true, maxLength: 100 }),
+        userName: ko.observable().extend({ required: true, maxLength: 100 }),
+        email: ko.observable().extend({ required: true, pattern: '^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$' }),
+        phoneNumber: ko.observable().extend({ required: true, pattern: '^01[0-2]{1}[0-9]{8}' }),
+        limitedCredit: ko.observable().extend({ required: true, pattern: '^[+-]?[0-9]{1,3}(?:[0-9]*(?:[.,][0-9]{2})?|(?:,[0-9]{3})*(?:\.[0-9]{2})?|(?:\.[0-9]{3})*(?:,[0-9]{2})?)$' }),
+        password: ko.observable()
+    });
+    ko.fileBindings.defaultOptions.buttonText = "Choose Image";
+    self.fileData = ko.observable({
+        file: ko.observable(), // will be filled with a File object
+        // Read the files (all are optional, e.g: if you're certain that it is a text file, use only text:
+        binaryString: ko.observable(), // FileReader.readAsBinaryString(Blob|File) - The result property will contain the file/blob's data as a binary string. Every byte is represented by an integer in the range [0..255].
+        text: ko.observable(), // FileReader.readAsText(Blob|File, opt_encoding) - The result property will contain the file/blob's data as a text string. By default the string is decoded as 'UTF-8'. Use the optional encoding parameter can specify a different format.
+        dataURL: ko.observable(), // FileReader.readAsDataURL(Blob|File) - The result property will contain the file/blob's data encoded as a data URL.
+        arrayBuffer: ko.observable(), // FileReader.readAsArrayBuffer(Blob|File) - The result property will contain the file/blob's data as an ArrayBuffer object.
 
+        // a special observable (optional)
+        base64String: ko.observable(), // just the base64 string, without mime type or anything else
+
+        // you can have observable arrays for each of the properties above, useful in multiple file upload selection (see Multiple file Uploads section below)
+        // in the format of xxxArray: ko.observableArray(),
+        /* e.g: */ fileArray: ko.observableArray(), base64StringArray: ko.observableArray()
+    });
     //TODO need to get the id from the logged in user
     self.customerId = ko.observable(1);
 
@@ -112,6 +138,76 @@
             self.init();
         }).fail(self.showError);
 
+    }
+    self.getCustomerById = function () {
+        console.log(self.customerId());
+        $.ajax({
+            type: 'Get',
+            url: '/api/Customer/' + self.customerId(),
+            contentType: 'application/json; charset=utf-8'
+        }).done(function (result) {
+            var data = result.customer;
+            console.log(data);
+            self.model().limitedCredit(data.LimitedCredit);
+            self.model().firstName(data.User.FirstName);
+            self.model().lastName(data.User.LastName);
+            self.model().email(data.User.Email);
+            self.model().userName(data.User.UserName);
+            self.model().password(data.User.PasswordHash);
+            self.model().phoneNumber(data.User.PhoneNumber);
+            self.fileData().dataURL('data:image/gif;base64,' + data.User.ImageData);
+            self.fileData().base64String(data.User.ImageData);
+        }).fail(self.showError);
+    };
+    self.getCustomerById();
+    //self.checkOut = function () {
+    //    console.log(self.credit());
+    //    console.log(self.total());
+    //    if (self.total() > self.credit()) {
+    //        alertify.error("Error,");
+    //        } else {
+    //        alertify.success("Done");
+    //        var result = self.credit() - self.total();
+    //        self.credit(result);
+    //        console.log(result);
+    //    }
+    //};
+
+    self.checkOut = function () {
+        if (self.total() < self.model().limitedCredit()) {
+           
+            var result = self.model().limitedCredit() - self.total();
+            self.model().limitedCredit(result);
+            console.log(result);
+            var data = {
+                id: self.customerId(),
+                user: {
+                    firstName: self.model().firstName(),
+                    lastName: self.model().lastName(),
+                    email: self.model().email(),
+                    userName: self.model().userName(),
+                    password: self.model().password(),
+                    phoneNumber: self.model().phoneNumber(),
+                    imageData: self.fileData().base64String()
+                },
+                limitedCredit: result
+            }
+            $.ajax({
+                type: 'PUT',
+                url: '/api/customer',
+                contentType: 'application/json; charset=utf-8',
+                data: JSON.stringify(data)
+            }).done(function (result) {
+                console.log(result);
+                alertify.success("Done");
+                }).fail(self.showError);
+        } else {
+            alertify.error("Error,");
+        }
+
+    }
+    self.cancel = function () {
+        document.location = '/Customer/Cafeteria/Index';
     }
 
 }
