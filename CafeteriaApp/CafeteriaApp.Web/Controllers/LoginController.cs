dcb9d2e62b6fa3e5af0cc2967a58f7e09ce7e713
ShaimaaHamdan <Shaimaa.Hamdan@gmail.com>
2017-07-08 00:41:32 +0200

    fix layout and login controller

--- CafeteriaApp/CafeteriaApp.Web/Controllers/LoginController.cs
+++ CafeteriaApp/CafeteriaApp.Web/Controllers/LoginController.cs
@@ -174,18 +174,6 @@ namespace CafeteriaApp.Web.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    //<<<<<<< HEAD
-                    if (Request.UrlReferrer.PathAndQuery != "/admin/user/create") // need authorization here
-                    {
-                        await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-                        return RedirectToAction("Index", "Cafeteria", new { area = "Customer" });
-                    }
-                    //await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    else
-                    {
-                        return RedirectToAction("Index", "User", new { area = "Admin" });
-                    }
-                    //=======
                     await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
                     //Create Customer and Assign User to Customer Role
@@ -199,16 +187,17 @@ namespace CafeteriaApp.Web.Controllers
 
                     appdb.Customers.Add(customer);
                     appdb.SaveChanges();
+
+                    return RedirectToAction("Index", "Cafeteria", new { area = "Customer" });
+
                 }
 
-                //>>>>>>> origin/master
                 // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                 // Send an email with this link
                 // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                 // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                 // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-                
-                
+
                 AddErrors(result);
             }
 
